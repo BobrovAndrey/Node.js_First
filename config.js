6129f1e32a8907bd267706a5025a558033f20210
Andrey Bobrov <Bobrov.law@gmail.com>
2018-11-19 17:46:14 +0200

    0.2.6

--- config.js
+++ config.js
@@ -12,14 +12,16 @@ let environments = {};
 environments.staging = {
   'httpPort' : 3000,
   'httpsPort': 3001,
-  'envName' : 'staging'
+  'envName' : 'staging',
+  'hasingSecret': 'thisIsASecret'
 };
 
 //Production environment
 environments.production = {
   'httpPort' : 5000,
   'httpsPort': 5001,
-  'envName' : 'production'
+  'envName' : 'production',
+  'hashingSecret': 'thisIsAlsoASecret'
 };
 
 //Determine which environment was passed
@@ -33,3 +35,36 @@ let environmentToExport = typeof(environments[currentEnvironment]) == 'object' ?
 //Export the module
 
 module.exports = environmentToExport;
+
+// /*
+//  * Create and export configuration variables
+//  *
+//  */
+
+// // Container for all environments
+// var environments = {};
+
+// // Staging (default) environment
+// environments.staging = {
+//   'httpPort' : 3000,
+//   'httpsPort' : 3001,
+//   'envName' : 'staging',
+//   'hashingSecret' : 'thisIsASecret'
+// };
+
+// // Production environment
+// environments.production = {
+//   'httpPort' : 5000,
+//   'httpsPort' : 5001,
+//   'envName' : 'production',
+//   'hashingSecret' : 'thisIsAlsoASecret'
+// };
+
+// // Determine which environment was passed as a command-line argument
+// var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// // Check that the current environment is one of the environments above, if not default to staging
+// var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// // Export the module
+// module.exports = environmentToExport;
